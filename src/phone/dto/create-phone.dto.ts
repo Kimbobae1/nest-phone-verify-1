b600e6107854b49528165bf39d1a3e6ae5bb1dcb
Bobae Kim <rlaqhqo0418@naver.com>
2024-09-27 17:00:21 +0900

    feat: 전화번호 포맷 예외처리

--- src/phone/dto/create-phone.dto.ts
+++ src/phone/dto/create-phone.dto.ts
@@ -1,9 +1,10 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsNotEmpty, IsString } from 'class-validator';
+import { IsNotEmpty, IsString, Matches } from 'class-validator';
 
 export class CreatePhoneDto {
   @IsNotEmpty()
   @IsString()
+	@Matches(/^010-\d{4}-\d{4}$/, {message: 'Phone number must be in the format 010-xxxx-xxxx'})
   @ApiProperty()
   phoneNumber : string;
 }
